58cb68d8e1aecc29bec607fc112aed1267685f20
Michaela07313 <miminka07313@gmail.com>
2017-04-13 14:07:54 +0300

    add calculate function

---
+++ Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Calculator_CSharp.Models;
+using System.Web.Mvc;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return this.RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            var result = 0m;
+
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+            }
+            return result;
+        }
+
+    }
+}
